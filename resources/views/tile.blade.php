94844f5b1c62118938d02421a42851e73a9ab024
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-04-23 15:45:12 +0200

    Styling

--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -1,29 +1,39 @@
 <x-dashboard-tile :position="$position" :show="$showTile">
-    <div class="grid gap-padding h-full markup" style="grid-template-rows: auto 1fr;">
-        <div class="flex">
-            <div class="grid place-center w-10 h-10 rounded-full"
-                 style="background-color: rgba(255, 255, 255, .9)">
-                <div class="text-3xl leading-none -mt-1">🚃</div>
+    <div class="grid grid-rows-auto-1 gap-2 h-full">
+        <div
+            class="flex items-center justify-center w-10 h-10 rounded-full"
+            style="background-color: rgba(255, 255, 255, .9)"
+        >
+            <div class="text-3xl leading-none -mt-1">
+                🚃
             </div>
-            <h1 class="ml-2"></h1>
         </div>
-        <div class="align-self-center grid gap-8" style="grid-auto-rows: auto;">
+        <div class="self-center | grid gap-8" style="grid-auto-rows: auto;">
             @foreach($trainConnections as $trainConnection)
-                <div>
-                    <h2 class="uppercase">{{ $trainConnection['label'] }}</h2>
-                    <ul class="mt-padding">
+                <div class="grid gap-2">
+                    <h2 class="uppercase">
+                        {{ $trainConnection['label'] }}
+                    </h2>
+                    <ul class="divide-y-2">
                         @foreach($trainConnection['trains'] as $train)
                             @if($loop->iteration <= 3)
-                                <li
-                                    class="{{ $train['canceled'] ? 'line-through text-danger' : '' }}"
-                                >
-                                    <span class="mr-2">{{ $train['station'] }}</span>
+                                <li class="
+                                    grid grid-cols-1-auto-auto py-1
+                                    {{ $train['canceled'] ? 'line-through text-danger' : '' }}
+                                ">
+                                    <span class="mr-2">
+                                        {{ $train['station'] }}
+                                    </span>
 
                                     @if(! $train['canceled'] && $train['delay'] > 0)
-                                        <span class="ml-auto mr-2 font-bold variant-tabular text-danger">{{ $train['delay'] }}m</span>
+                                        <span class="ml-auto mr-2 font-bold variant-tabular text-danger">
+                                            {{ $train['delay'] }}m
+                                        </span>
                                     @endif
-                                    <span
-                                        class="flex-none font-bold text-right variant-tabular">{{ \Carbon\Carbon::createFromTimestamp($train['time'])->format('H:i') }}</span>
+
+                                    <span class="flex-none font-bold text-right variant-tabular">
+                                        {{ \Carbon\Carbon::createFromTimestamp($train['time'])->format('H:i') }}
+                                    </span>
                                 </li>
                             @endif
                         @endforeach
